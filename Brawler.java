a02f57956180c8e1f73335640138ca1107662f29
Gleb Minor <spukebrawlstars@gmail.com>
2026-09-13 01:46:50 +0500

    added README, Brawler class added

---
+++ Brawler.java
@@ -0,0 +1,25 @@
+public class Brawler {
+    private String name;
+    private String[] base_attack;
+    private String[] super_attack;
+    private String[] gadgets;
+    private String[] gears;
+    private String[] star_powers;
+    private String hupercharge;
+    private String[] traits;
+
+    public Brawler(String name, String[] base_attack, String[] super_attack, String[] gadgets, String[] gears, String[] star_powers, String hupercharge, String[] traits) {
+        this.name = name;
+        this.base_attack = base_attack;
+        this.super_attack = super_attack;
+        this.gadgets = gadgets;
+        this.gears = gears;
+        this.star_powers = star_powers;
+        this.hupercharge = hupercharge;
+        this.traits = traits;
+    }
+
+    publick void setHypercharge(String hypercharge) {
+        this.hupercharge = hypercharge;
+    }
+}
